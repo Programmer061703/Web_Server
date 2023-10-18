657d53abb630658b47d5c059785b83e88868a6c3
Blake Williams <blaker1703@gmail.com>
2023-10-18 13:50:03 -0500

    Added: Update method

--- back_end/main.py
+++ back_end/main.py
@@ -1,9 +1,60 @@
-from typing import Mapping, Any
+from typing import Mapping, Any, List, Tuple, Dict
 import os
 from http_daemon import delay_open_url, serve_pages
 
-def make_ajax_page(params: Mapping[str, Any]) -> Mapping[str, Any]:
-    print(f'make_ajax_page was called with {params}')
+class Player():
+        def __init__(self, id:str) -> None:
+            self.id = id
+            self.x = 0
+            self.y = 0
+            self.known = 0
+
+players: Dict[str, Player] = {}
+history: List[Player] = []
+
+# Find a player by id, creating it if necessary
+def find_player(id: str) -> Player:
+    if id not in players:
+        players[id] = Player(id)
+       
+        
+
+    return players[id]
+
+
+
+    
+
+
+
+
+
+
+
+def update(payload: Mapping[str, Any]) -> Mapping[str, Any]:
+    action = payload['action']
+    if action == 'clicked':
+         player = find_player(payload["id"])
+         player.x = payload["x"]
+         player.y = payload["y"]
+         history.append(player)
+         print(f'Created player {id}')
+    elif action == 'updates':
+         player = find_player(payload["id"])
+         remaining_history = history[player.known:]
+         player.known = len(history)
+         updates: List[Tuple[str, int, int]] = {}
+         for i in range(len(remaining_history)):
+            player = remaining_history[i]
+            updates.append((player.id, player.x, player.y))
+         return {
+             'updates': updates,
+         }
+    
+
+
+
+    print(f'make_ajax_page was called with {payload}')
     return {
         'message': 'yo momma',
     }
@@ -16,7 +67,7 @@ def main() -> None:
     port = 8987
     delay_open_url(f'http://localhost:{port}/game.html', .1)
     serve_pages(port, {
-        'ajax.html': make_ajax_page,
+        'ajax.html': update,
     })
 
 if __name__ == "__main__":
